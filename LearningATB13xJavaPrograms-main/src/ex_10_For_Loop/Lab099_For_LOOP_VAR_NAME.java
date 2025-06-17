8bf482ffa24e7d4666305ee6c310ddab8c835df6
Raksha Agrawal <66050508+raksha01@users.noreply.github.com>
2025-06-17 23:05:54 +0530

    Delete LearningATB13xJavaPrograms-main/src/ex_10_For_Loop directory

--- LearningATB13xJavaPrograms-main/src/ex_10_For_Loop/Lab099_For_LOOP_VAR_NAME.java
+++
@@ -1,9 +0,0 @@
-package ex_10_For_Loop;
-
-public class Lab099_For_LOOP_VAR_NAME {
-    public static void main(String[] args) {
-        for (int aditi = 0; aditi < 10; aditi++) {
-            System.out.println(aditi);
-        }
-    }
-}
